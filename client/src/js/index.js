c7a7bb000aa7664e59dd10636c0c8512cdd2ea6b
Kevin Reid <kevinnivek@me.com>
2022-09-04 16:17:02 -0400

    added read and create

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -15,7 +15,12 @@ import Logo from "../images/logo.png";
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
+// import initDb, getDb, and PostDB function
+import { initDb, getDb, postDb } from './database';
+
 window.addEventListener('load', function () {
+  initDb();
+
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
