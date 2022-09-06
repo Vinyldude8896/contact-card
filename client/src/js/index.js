e91f80ee1b365b7cf722024d98bc091ef6864f6a
Kevin Reid <kevinnivek@me.com>
2022-09-06 10:35:58 -0400

    added service worker

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,37 +1,35 @@
-// Import modules
-import  "./form";
+// import modules
+import "./form";
+// import "./submit";
 
-// Import CSS files
+// import css files
 import "../css/index.css";
-
-// Import bootstrap
 import { Tooltip, Toast, Popover } from 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-
-// Import images
-import Logo from "../images/logo.png";
+// import images
+import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
+// import database
+import { initdb, getDb, postDb, editDb, deleteDb } from './database';
 
-// import initDb, getDb, and PostDB function
-import { initDb, getDb, editDb, postDb, deleteDb } from './database';
-
+// import card
 import { fetchCards } from './cards';
-
+// import form
 import { toggleForm, clearForm } from './form';
 
-window.addEventListener('load', function() {
-  initDb();
+// add images on load
+window.addEventListener('load', function () {
+  initdb();
   fetchCards();
+
   document.getElementById('logo').src = Logo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
-})
-
-
-          // Form functionality
+});
+// Form functionality
 const form = document.getElementById("formToggle");
 const newContactButton = document.getElementById("new-contact");
 let submitBtnToUpdate = false;
@@ -39,65 +37,58 @@ let profileId;
 
 newContactButton.addEventListener('click', event => {
   toggleForm()
- })
-
-
+})
 
 form.addEventListener('submit', event => {
   // Handle data
   event.preventDefault();
-let name = document.getElementById("name").value;
-let phone = document.getElementById("phone").value;
-let email = document.getElementById("email").value;
-let profile = document.querySelector('input[type="radio"]:checked').value;
+  let name = document.getElementById("name").value;
+  let phone = document.getElementById("phone").value;
+  let email = document.getElementById("email").value;
+  let profile = document.querySelector('input[type="radio"]:checked').value;
 
   // Post form data to IndexedDB OR Edit an existing card in IndexedDB
-if (submitBtnToUpdate == false) {
-  postDb(name, email, phone, profile);
-} else {
-  // Obtains values passed into the form element
-let name = document.getElementById("name").value;
-let phone = document.getElementById("phone").value;
-let email = document.getElementById("email").value;
-let profile = document.querySelector('input[type="radio"]:checked').value;
-// Calls the editDB function passing in any values from the form element as well as the ID of the contact that we are updating
-editDb(profileId, name, email, phone, profile);
+  if (submitBtnToUpdate == false) {
+    postDb(name, email, phone, profile);
+  } else {
+    // Obtains values passed into the form element
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+    let profile = document.querySelector('input[type="radio"]:checked').value;
+    // Calls the editDB function passing in any values from the form element as well as the ID of the contact that we are updating
+    editDb(profileId, name, email, phone, profile);
 
-  fetchCards();
+    fetchCards();
     // Toggles the submit button back to POST functionality
-  submitBtnToUpdate = false;
-}
+    submitBtnToUpdate = false;
+  }
 
-// Clear form
-clearForm();
-// Toggle form
-toggleForm();
-// Reload the DOM
-fetchCards();
+  // Clear form
+  clearForm();
+  // Toggle form
+  toggleForm();
+  // Reload the DOM
+  fetchCards();
 });
 
 window.deleteCard = (e) => {
-  // Grabs the id from the button element attached to the contact card.
+  // grabs the id from the button element attached to the contact card
   let id = parseInt(e.id);
-  // Delete the card
+  // delete the card
   deleteDb(id);
-  // Reload the DOM
+  // reload the DOM
   fetchCards();
 };
 
 window.editCard = (e) => {
-  // Grabs the id from the button element attached to the contact card and sets a global variable that will be used in the form element.
+  // grabs the id from the button element attached to the contact card and sets a golbal variable that will be used in the form element.
   profileId = parseInt(e.dataset.id);
 
-  // Grabs information to pre-populate edit form
-  // let editName = e.dataset.name;
-  // let editEmail = e.dataset.email;
-  // let editPhone = e.dataset.phone;
-
-    // Grabs information to pre-populate edit form
-  let editName = e.name;
-  let editEmail = e.email;
-  let editPhone = e.phone;
+  // grabs information to pre-populate edit form
+  let editName = e.dataset.name;
+  let editEmail = e.dataset.email;
+  let editPhone = e.dataset.phone;
 
   document.getElementById("name").value = editName;
   document.getElementById("email").value = editEmail;
@@ -106,16 +97,12 @@ window.editCard = (e) => {
   form.style.display = "block";
 
   // Toggles the Submit button so that it now Updates an existing contact instead of posting a new one
-    submitBtnToUpdate = true;
-
-      // Delete the card
-  editDb(profileId);
-  // Reload the DOM
-  fetchCards();
-};
+  submitBtnToUpdate = true;
+}
 
 if ('serviceWorker' in navigator) {
-  // Use the window load event to keep the page load performant
+  // use the window load event to keep the page load performant
   window.addEventListener('load', () => {
-  navigator.serviceWorker.register('./service-worker.js');
-})};
+    navigator.serviceWorker.register('./service-worker.js');
+  })
+};
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,51 +1,6 @@
-// const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const WorkboxPlugin = require('workbox-webpack-plugin');
-
-// module.exports = {
-//   mode: 'development',
-//   entry: './src/js/index.js',
-//   output: {
-//     filename: 'bundle.js',
-//     path: path.resolve(__dirname, 'dist'),
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-//         type: 'asset/resource',
-//       },
-//       {
-//         test: /\.css$/i,
-//         use: ['style-loader', 'css-loader'],
-//       },
-//       {
-//         test: /\.m?js$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: 'babel-loader',
-//           options: {
-//             presets: [
-//               ['@babel/preset-env', { targets: "defaults" }]
-//             ]
-//           }
-//         }
-//       }
-//     ],
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: './index.html',
-//       title: 'Webpack Plugin',
-//     }),
-//     new WorkboxPlugin.GenerateSW()
-//   ]
-// };
-
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const {InjectManifest} = require('workbox-webpack-plugin');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {InjectManifest} = require('workbox-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -54,7 +9,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-    module: {
+  module: {
     rules: [
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -78,52 +33,14 @@ module.exports = {
       }
     ],
   },
-
-plugins: [
-  new HtmlWebpackPlugin({
-    template: './index.html',
-    title: 'Webpack Plugin',
-  }),
-  new InjectManifest({
-    // swSrc: './sw.js',
-    swSrc: './src/sw.js',
-    swDest: 'service-worker.js',
-  }),
-],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'Webpack Plugin',
+    }),
+    new InjectManifest({
+      swSrc: './src/sw.js',
+      swDest: 'service-worker.js',
+    }),
+  ],
 };
-
-module.exports = {
-  plugins: [new MiniCssExtractPlugin()],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-    ],
-  },
-};
-
-// new WorkboxPlugin.GenerateSW({
-//   // Do not precache images
-//   exclude: [/\.(?:png|jpg|jpeg|svg)$/],
-
-//   // Define runtime caching rules.
-//   runtimeCaching: [{
-//     // Match any request that ends with .png, .jpg, .jpeg or .svg.
-//     urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
-
-//     // Apply a cache-first strategy.
-//     handler: 'CacheFirst',
-
-//     options: {
-//       // Use a custom cache name.
-//       cacheName: 'images',
-
-//       // Only cache 1 images
-//       expiration: {
-//         maxEntries: 1,
-//       },
-//     },
-//   }],
-// })
